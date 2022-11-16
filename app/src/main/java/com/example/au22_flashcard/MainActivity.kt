b4182658a93a979d29714e900c820412f08606e3
ciliaence <cilia.ence@telia.com>
2022-11-16 15:45:43 +0100

    Added removeWordsActivity

--- app/src/main/java/com/example/au22_flashcard/MainActivity.kt
+++ app/src/main/java/com/example/au22_flashcard/MainActivity.kt
@@ -6,18 +6,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.widget.Button
-import android.widget.TextView
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
 
-    lateinit var wordButton : Button
-    var currentWord : Word? = null
-    lateinit var db : AppDatabase
-    private lateinit var job : Job
+    lateinit var db: AppDatabase
+    private lateinit var job: Job
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
+    private lateinit var flashCardButton: Button
+    var currentWord: Word? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +26,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         db = AppDatabase.getInstance(this)
         job = Job()
 
-        wordButton = findViewById(R.id.wordButton)
-
-
-        showNewWord()
-
-        wordButton.setOnClickListener {
+        flashCardButton = findViewById(R.id.flashCardButton)
+        flashCardButton.setOnClickListener {
             revealTranslation()
         }
 
@@ -41,59 +37,51 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
             startActivity(intent)
         }
 
-        val list  = loadAllItems()
+        val removeWordsButton = findViewById<Button>(R.id.removeWordsButton)
+        removeWordsButton.setOnClickListener {
+            val intent = Intent (this, RemoveWordsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val list = loadAllItems()
 
         launch {
 
             val wordList = list.await()
-
             WordList.wordList.addAll(wordList)
-
-
-            for(word in wordList) {
+            for (word in wordList) {
 
                 Log.d("!!!", "word: ${word.english}")
             }
         }
-
-
+        showNewWord()
     }
 
-
-
-    private fun loadAllItems() : Deferred<List<Word>> =
-        async (Dispatchers.IO) {
+    private fun loadAllItems(): Deferred<List<Word>> =
+        async(Dispatchers.IO) {
             db.wordDao.getAll()
         }
 
     private fun revealTranslation() {
-        if(wordButton.text == currentWord?.swedish){
-            wordButton.text = currentWord?.english
-        }else{
-            wordButton.text = currentWord?.swedish
+        if (flashCardButton.text == currentWord?.swedish) {
+            flashCardButton.text = currentWord?.english
+        } else {
+            flashCardButton.text = currentWord?.swedish
         }
-
     }
-
 
     private fun showNewWord() {
 
         currentWord = WordList.getNewWord()
-        wordButton.text = currentWord?.swedish
+        flashCardButton.text = currentWord?.english
     }
-
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
         if (event?.action == MotionEvent.ACTION_UP) {
             showNewWord()
         }
-
         return true
     }
 }
-
-//1. skapa en ny activity där ett nytt ord får skrivas in
-//2. spara det nya ordet i databasen
-//3. I main activity läs in alla ord från databasen
-//använd coroutiner när ni läser och skriver till databasen se tidigare exempel
--- app/src/main/java/com/example/au22_flashcard/WordList.kt
+++ app/src/main/java/com/example/au22_flashcard/WordList.kt
@@ -1,46 +1,29 @@
 package com.example.au22_flashcard
 
 object WordList {
-     val wordList = mutableListOf<Word>()
+    val wordList = mutableListOf<Word>()
     private val usedWords = mutableListOf<Word>()
 
     init {
-        initializeWords()
+        wordList.add(Word(0, "Hello", "Hej"))
     }
 
-
-    fun initializeWords() {
-        val word = Word(0, "Hello", "Hej")
-        wordList.add(word)
-        wordList.add(Word(0, "Good bye", "Hej då"))
-        wordList.add(Word(0, "Thank you", "Tack"))
-        wordList.add(Word(0, "Welcome", "Välkommen"))
-        wordList.add(Word(0, "Computer", "Dator"))
-
-    }
-
-    fun getNewWord() : Word {
+    fun getNewWord(): Word {
         if (wordList.size == usedWords.size) {
             usedWords.clear()
         }
 
-        var word : Word? = null
+        var word: Word? = null
 
         do {
             val rnd = (0 until wordList.size).random()
             word = wordList[rnd]
-        } while(usedWords.contains(word))
+        } while (usedWords.contains(word))
 
         usedWords.add(word!!)
 
         return word
     }
-
-    // 1. en till lista med redan använda ord
-    // 2. lista med index på använda ord
-    // 3. använt ord tas bort från listan
-    // 4. ordet håller reda på om det redan är använt
-
 }
 
 
